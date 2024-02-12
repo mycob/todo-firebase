c0e0fdcbb3598705491c79eda6688603e396b053
mycomike <mycoberago@gmail.com>
2024-02-12 09:17:56 -0600

    fixed wrong imported component

--- app/todo-app/layout.tsx
+++ app/todo-app/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import useAuth from '../lib/hooks/useAuth';
-import SignInComponent from './@signin/page';
+import SignInComponent from '../components/signin/signin';
 
 const ToDoAppLayoutComponent = ({ children } : 
     { children: React.ReactElement }) => {
