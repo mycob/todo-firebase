315fe98330ca09dca12f46795955e59d6d035e1f
mycomike <mycoberago@gmail.com>
2024-02-19 06:58:26 -0600

    configure environment variables

--- app/lib/firebase/clientApp.js
+++ app/lib/firebase/clientApp.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvr-QyOL3SVc9IEhkc-qICUT83dsCRnA0",
-  authDomain: "todo-firebase-19c22.firebaseapp.com",
-  projectId: "todo-firebase-19c22",
-  storageBucket: "todo-firebase-19c22.appspot.com",
-  messagingSenderId: "841113620295",
-  appId: "1:841113620295:web:92462662e58197ab640662",
-  measurementId: "G-JZ9KEM908V"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT
 };
 
 // Initialize Firebase
