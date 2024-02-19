a805e4b28f122cf2d3ee6582715cc8150d701ce0
mycomike <mycoberago@gmail.com>
2024-02-19 07:50:20 -0600

    Updated Front Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-36">
-      <h1>Welcome to my ToDo App Yo 🤗</h1>
+      <h1>ToDo App with NextJS 14 & Firebase🤗</h1>
       <Link href='/todo-app'>
         <button
           className="text-blue-600 bg-white rounded-md px-2 py1 mt-6"
