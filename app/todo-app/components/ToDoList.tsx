14f3f032aa5303a4ec1ee972908dbdea95a21f76
mycomike <mycoberago@gmail.com>
2024-02-29 12:53:55 -0600

    Added the ability to filter to do list

--- app/todo-app/components/ToDoList.tsx
+++ app/todo-app/components/ToDoList.tsx
@@ -7,6 +7,8 @@ import ToDoItemComponent from './ToDoItem';
 
 const ToDoListComponent = () => {
     const [ todos, setToDos ] = useState<any []>([]);
+    const [ visibleTodos, setVisibleTodos ] = useState<any []>([]);
+    const [ filter, setFilter ] = useState<string>('all-todos');
     const auth = useAuth();
 
     useEffect(() => {
@@ -27,14 +29,78 @@ const ToDoListComponent = () => {
         return () => unsubscribe();
     },[ auth ])
 
+    useEffect(() => {
+        setVisibleTodos(todos);
+    },[todos])
+
+    useEffect(() => {
+        switch(filter) {
+            case 'all-todos':
+                setVisibleTodos(todos);
+                return;
+            case 'pending':
+                let allPending = todos.filter(todo => !todo.complete);
+                setVisibleTodos(allPending);
+                return;
+            case 'complete':
+                let allComplete = todos.filter(todo => todo.complete);
+                setVisibleTodos(allComplete);
+                return;
+            default:
+                setVisibleTodos(todos);
+        }
+    },[filter,todos])
+
     if(todos.length <= 0) {
         return (<h1 className='text-center'>You&apos;re all caught up.  Nice work! 🎉</h1>)
+    }
+
+    const handleFilter = (e: any) => {
+        let filterSelected = e.currentTarget.id;
+        setFilter(filterSelected);
     }
     
     return (
         <div className='flex flex-col items-center'>
+            <div>
+                <h1 className='text-center mb-2 font-bold'>Filters</h1>
+                
+                <input type='radio' 
+                    className='mx-2'
+                    id='pending'
+                    name='filter'
+                    checked={filter == 'pending'}
+                    onChange={handleFilter}
+                />
+                 <label
+                    htmlFor='pending'
+                 >Pending</label>
+
+                <input type='radio' 
+                    className='mx-2'
+                    id='complete'
+                    name='filter'
+                    checked={filter == 'complete'}
+                    onChange={handleFilter}
+                />
+                 <label
+                    htmlFor='complete'
+                 >Complete</label>
+
+                <input type='radio' 
+                    className='mx-2'
+                    id='all-todos'
+                    name='filter'
+                    checked={filter == 'all-todos'}
+                    onChange={handleFilter}
+                />
+                 <label
+                    htmlFor='all-todos'
+                 >All To Dos</label>
+
+            </div>
             <div className='p-3 min-w-fit w-[350px]'>
-                {todos.map((todo) => (
+                {visibleTodos.map((todo) => (
                     <ToDoItemComponent key={todo.id} todo={todo} />
                 ))} 
             </div>
